2e39eb821ee12412ece08097930967ea3ae69f5d
ryamada-cmd <r.yamada@river-sides.com>
2026-08-24 21:37:48 +0900

    security: restrict auth trigger function execution

---
+++ supabase/migrations/0003_restrict_auth_trigger_function.sql
@@ -0,0 +1,3 @@
+revoke all on function public.handle_new_user() from public;
+revoke all on function public.handle_new_user() from anon;
+revoke all on function public.handle_new_user() from authenticated;
