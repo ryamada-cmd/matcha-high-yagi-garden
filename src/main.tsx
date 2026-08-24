c8349f477b8f367891a54108f4e283d408d26d42
ryamada-cmd <r.yamada@river-sides.com>
2026-08-24 23:14:17 +0900

    fix: load desktop layout override for spray sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './features.css'
+import './layout-fixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
