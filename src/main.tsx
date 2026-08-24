2cbb2f4c48c1e81170649f53f4b764e773affed8
ryamada-cmd <r.yamada@river-sides.com>
2026-08-25 02:41:28 +0900

    Load admin console styles

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './inventory-admin.css'
 import './spray-guidance.css'
 import './spray-history.css'
 import './dashboard-ops.css'
+import './admin-console.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
