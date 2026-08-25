5248a15dd9b1c562696beaaa66dbf7a85dc6023c
ryamada-cmd <r.yamada@river-sides.com>
2026-08-25 09:47:56 +0900

    Load fertilizer dashboard styles

--- src/main.tsx
+++ src/main.tsx
@@ -16,6 +16,7 @@ import './mobile-field.css'
 import './brand-logo.css'
 import './weather-dashboard.css'
 import './fertilizer.css'
+import './fertilizer-dashboard.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
